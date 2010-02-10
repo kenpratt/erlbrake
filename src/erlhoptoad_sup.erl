122cf396e96b2bada753b934b925844cc03a9fad
Ken Pratt <ken@kenpratt.net>
2010-02-09 18:00:29 -0800

    Added Hoptoad to app supervisor.

--- src/erlhoptoad_sup.erl
+++ src/erlhoptoad_sup.erl
@@ -1,4 +1,3 @@
-
 -module(erlhoptoad_sup).
 
 -behaviour(supervisor).
@@ -10,7 +9,7 @@
 -export([init/1]).
 
 %% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+-define(CHILD(I, Type, Opts), {I, {I, start_link, Opts}, permanent, 5000, Type, [I]}).
 
 %% ===================================================================
 %% API functions
@@ -24,5 +23,5 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
-
+    Hoptoad = ?CHILD(hoptoad, worker, ["production", "76fdb93ab2cf276ec080671a8b3d3866"]),
+    {ok, { {one_for_one, 5, 10}, [Hoptoad]} }.
