aff0c14a79c07ad2530691f1da3c33250178fe93
Stephan Zeissler <stephan.zeissler@moinz.de>
2010-02-12 14:02:16 +0100

    Added error case, when no apikey is given while starting the erlhoptoad application

--- src/erlhoptoad_app.erl
+++ src/erlhoptoad_app.erl
@@ -11,14 +11,24 @@
 
 start(_StartType, _StartArgs) ->
     {ok, Environment} = application:get_env(environment),
-    {ok, ApiKey} = application:get_env(apikey),
-    case application:get_env(error_logger) of
-       {ok, true} ->
-             error_logger:add_report_handler(erlhoptoad_error_logger);
-       _ -> ok
-    end,
 
-    erlhoptoad_sup:start_link(Environment, ApiKey).
-
+    case application:get_env(apikey) of
+       {ok, "ENTER_API_KEY"} -> {error, no_apikey};
+       {ok, Api} -> 
+       
+            case application:get_env(error_logger) of
+                {ok, true} ->
+                      error_logger:add_report_handler(erlhoptoad_error_logger);
+                _ -> ok
+            end,
+             
+            erlhoptoad_sup:start_link(Environment, Api);
+       
+       
+       
+       undefined -> {error, no_apikey}
+    end.
+    
+    
 stop(_State) ->
     ok.
